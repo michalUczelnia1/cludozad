6d42068b7f77e235b0686c862d0e5415d2f5e70c
michalUczelnia1 <64002841+michalUczelnia1@users.noreply.github.com>
2021-02-14 23:39:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+            foreach (Person p1 in people)
+            {
+                if (p1.Age<=20)
+                {
+                    foreach (Company c1 in companies)
+                    {
+                        if (c1.ID==p1.CompanyID)
+                        {
+                            companies.Remove(c1);
+                        }
+                    }
+                }
+            }
